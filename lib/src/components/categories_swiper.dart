43a8ba2d6f20aadf4447706fe87f08daf68b49d6
MangelGP <mangel1098@outlook.com>
2020-09-01 17:49:32 -0500

    Carrusel de categorias

---
+++ lib/src/components/categories_swiper.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_oso_test/src/models/categories_model.dart';
+import 'package:flutter_oso_test/src/providers/user_preferences.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+
+class CategoriesSwiper extends StatelessWidget {
+
+  final List<Categoria> categorias;
+  final prefs = new UserPreferences();
+
+  CategoriesSwiper({Key key, this.categorias}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      width: double.infinity,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return _crearCategoria(context, categorias[index]);
+        },
+        itemCount: categorias.length,
+        autoplay: true,
+        autoplayDelay: 5000,
+        control: new SwiperControl(),
+        viewportFraction: 0.8,
+        scale: 0.8,
+      ),
+    ); 
+  }
+
+  Widget _crearCategoria(BuildContext context ,Categoria categoria) {
+
+    final caratulaCategoria = Container(
+      height: double.infinity,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: FadeInImage(
+          placeholder: AssetImage('assets/img/loading2.gif'), 
+          image: NetworkImage(categoria.getImg()),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+
+    final card = Stack(
+      children: [
+        caratulaCategoria,
+        Center(
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(5.0),
+              color: Colors.grey[700].withOpacity(0.5),
+            ),
+            padding: EdgeInsets.all(5.0),
+            child: Text(
+              categoria.descripcion, 
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white, 
+                fontWeight: FontWeight.bold, 
+                fontSize: 40.0
+              )
+            ),
+          )
+        )
+      ],
+    );
+
+    return GestureDetector(
+      child: card,
+      onTap: () {
+        prefs.idCategoria = categoria.id;
+        Navigator.pushNamed(context, 'products_by_cat', arguments: categoria);
+      },
+    );
+  }
+}
--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,39 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_oso_test/src/components/categories_swiper.dart';
 
 import 'package:flutter_oso_test/src/components/my_drawer.dart';
 import 'package:flutter_oso_test/src/providers/user_preferences.dart';
 
+import '../components/search_delegate.dart';
+import '../constants/constants.dart';
+import '../providers/categories_provider.dart';
+
 class HomePage extends StatelessWidget {
 
   final prefs = new UserPreferences();
+  final catProvider = new CategoriasProvider();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Osoonline'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: (){
-              Navigator.pushNamed(context, 'shopping_cart');
-            }       
-          ),
-          SizedBox(width: 10.0),
-          Icon(Icons.favorite),
-          SizedBox(width: 10.0),
-        ],
-      ),
+      appBar: _buildAppBar(context),
+      body: _buildBody(context),
       drawer: MyDrawer(),
-      body: Container(
-        child: Center(
-          child: RaisedButton(
-            child: Text('Categorias'),
-            onPressed: ()=> Navigator.pushNamed(context, 'cat_page')
-          ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return ListView(
+      children: [
+        SizedBox(height: kDefaultPaddin,),
+        FutureBuilder(
+          future: catProvider.getAllCategorias(),
+          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+            if (snapshot.hasData) {
+              return CategoriesSwiper(
+                categorias: snapshot.data
+              );
+            } else {
+              return Container(
+                height: 400.0,
+                child: Center(
+                  child: CircularProgressIndicator()
+                )
+              );
+            }  
+          },
         ),
-      ),
+      ],
+    );
+  }
+
+  // Container _buildBody(BuildContext context) {
+  //   return Container(
+  //     child: Center(
+  //       child: RaisedButton(
+  //         child: Text('Categorias'),
+  //         onPressed: ()=> Navigator.pushNamed(context, 'cat_page')
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: Text('Osoonline'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            showSearch(context: context, delegate: DataSearch());
+          }
+        ),
+        IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: (){
+            Navigator.pushNamed(context, 'shopping_cart');
+          }       
+        ),
+        SizedBox(width: 10.0),
+        Icon(Icons.favorite),
+        SizedBox(width: 10.0),
+      ],
     );
   }
 }
