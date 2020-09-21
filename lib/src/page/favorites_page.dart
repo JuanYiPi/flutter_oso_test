57cc315e80dddf697c8e6fffe48e6b7fa0c1dc1e
Miguel Angel Gomez Perez <mangel1098@outlook.com>
2020-09-21 12:30:42 -0500

    list favorites

--- lib/src/page/favorites_page.dart
+++ lib/src/page/favorites_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_oso_test/src/components/my_product_card.dart';
+import 'package:flutter_oso_test/src/models/product_model.dart';
 import 'package:flutter_oso_test/src/providers/favorites_provider.dart';
 
 
@@ -9,19 +11,23 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton(
-              onPressed: () {
-                favProvider.getFavorites();
-              }, 
-              child: Text('Favoritos')
-            )
-          ],
-        )
+      appBar: AppBar(
+        title: Text('Mis favoritos')
       ),
+      body: FutureBuilder(
+        future: favProvider.getFavorites(),
+        builder: (BuildContext context, AsyncSnapshot<List<Product>> snapshot) {
+          if (snapshot.hasData) {
+            final products = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, index) => MyProductCard(product: products[index]),
+              itemCount: products.length,
+            );
+          } else {
+            return Center(child: CircularProgressIndicator(),);
+          }
+        },
+      ),      
     );
   }
 }
